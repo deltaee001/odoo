13160b48b62d8b65769d5d0700206ad25486fdd1
deltaee001 <91780612+deltaee001@users.noreply.github.com>
2024-02-14 15:18:20 +0530

    Update __manifest__.py

--- addons/price_surcharge/__manifest__.py
+++ addons/price_surcharge/__manifest__.py
@@ -4,9 +4,9 @@
     'version': '1.0',
     'depends': ['base', 'sale'],
     'data': [
-        'security/ir.model.access.csv',
         'views/sale_order_view.xml',
-        'views/surcharge_mapping.xml'
+        'views/surcharge_mapping.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'auto_install': False,
